45244eb6fc0e7d09dfbd5bc47d5b36b73f3f898c
Pavel <Pavel-bfl@yandex.ru>
2023-01-28 21:15:51 +0500

    Добавлены база работы с сущьностями

---
+++ StepFlow.Entities/DomainBase.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Numerics;
+
+namespace StepFlow.Entities
+{
+	public class DomainBase
+	{
+		[DatabaseGenerated(DatabaseGeneratedOption.None)]
+		public int? Id { get; set; }
+	}
+}
---
+++ StepFlow.Entities/FlowContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StepFlow.Entities
+{
+	public class FlowContext : DbContext
+	{
+		private const string FILE_NAME = "Mobile.db";
+		private const string CONNECTION_STRING = "Filename=" + FILE_NAME;
+
+		public FlowContext()
+		{
+			Database.EnsureCreated();
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite(CONNECTION_STRING);
+		}
+	}
+}
