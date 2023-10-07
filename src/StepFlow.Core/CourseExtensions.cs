9f8f4631af9f9a135ac5500033845ddf414d8df8
Pavel <Pavel-bfl@yandex.ru>
2023-10-07 20:41:53 +0500

    Реализован алгоритм конвертации отрезка в пиксели

--- src/StepFlow.Core/CourseExtensions.cs
+++ src/StepFlow.Core/CourseExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
 using StepFlow.Common.Exceptions;
 
 namespace StepFlow.Core
@@ -30,5 +33,62 @@ namespace StepFlow.Core
 			Course.LeftBottom => Course.RightTop,
 			_ => throw EnumNotSupportedException.Create(course),
 		};
+
+		public static IEnumerable<Point> AllLine(Point first, Point second)
+		{
+			if (first == second)
+			{
+				yield return first;
+			}
+			else if (Math.Abs(first.X - second.X) > Math.Abs(first.Y - second.Y))
+			{
+				foreach (var point in Line(first, second))
+				{
+					yield return point;
+				}
+
+				yield return second;
+			}
+			else
+			{
+				foreach (var point in Line(Reflect(first), Reflect(second)))
+				{
+					yield return Reflect(point);
+				}
+
+				yield return second;
+			}
+		}
+
+		private static Point Reflect(Point point) => new Point(point.Y, point.X);
+
+		private static IEnumerable<Point> Line(Point first, Point second)
+		{
+			var delta = new Point(
+				Math.Abs(second.X - first.X),
+				Math.Abs(second.Y - first.Y)
+			);
+
+			var error = 0;
+			var deltaError = delta.Y;
+			var y = first.Y;
+			var step = Math.Sign(second.Y - first.Y);
+
+			var result = new List<Point>();
+
+			var stepX = Math.Sign(second.X - first.X);
+			for (var iX = first.X; iX != second.X; iX += stepX)
+			{
+				result.Add(new Point(iX, y));
+				error += deltaError;
+
+				if (error >= delta.X)
+				{
+					y += step;
+					error -= delta.X;
+				}
+			}
+			return result;
+		}
 	}
 }
